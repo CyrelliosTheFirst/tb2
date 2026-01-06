02acb5a1bd0907366fd2eea93c01dcb4d02a5f82
nmcdo <n.mcdorman@outlook.com>
2026-01-06 16:49:39 -0500

    Tweaking Theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,15 +8,16 @@ import './index.css'
 
 const App = () => {
   return (    
-    <div data-theme="coffee" className="relative h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#C3C3DA_100%)]" />      
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-      <Route path="/note/:id" element={<NoteDetailPage />} />
-      </Routes>
-    </div>
-    
+    <div >
+      <div className="relative h-full w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_30%,#2E202D_100%)]"/>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+        </Routes>
+      </div>
+    </div>    
   )
 };
 
